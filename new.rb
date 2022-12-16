6e46ae0f54500ea611d4bb5b4db06e4106ffd65c
Sakthivel M <sakthivel@innoventes.co>
2022-12-16 12:34:53 +0530

    ==array methods and it uses==

--- new.rb
+++ new.rb
@@ -21,17 +21,52 @@
 
 #addition of two numbers
 
-puts ("Enter the number1")
+# puts ("Enter the number1")
 
-number1 = gets.chomp().to_i
+# number1 = gets.chomp().to_i
 
-puts ("Enter the number2")
+# puts ("Enter the number2")
 
 
-number2 = gets.chomp().to_i
+# number2 = gets.chomp().to_i
 
-# puts(number1 + number2)
+# # puts(number1 + number2)
  
-res = number1 + number2;
+# res = number1 + number2;
 
-puts ("The result is: " + res.to_s)
+# puts ("The result is: " + res.to_s)
+
+#arrays in Ruby
+
+fruits = ['apple', 'orange', 'pineapple', 'mango']
+
+fruits.pop
+
+puts (fruits)
+
+fruits.shift
+
+puts ('--------------------------------------------------------')
+
+puts (fruits)
+
+fruits.push('kiwi');
+puts ('--------------------------------------------------------')
+
+puts (fruits)
+
+fruits.unshift('strawberry');
+puts ('--------------------------------------------------------')
+
+puts (fruits)
+
+
+fruits.delete('strawberry');
+puts ('--------------------------------------------------------')
+
+puts (fruits)
+
+
+puts ('--------------------------------------------------------')
+
+puts (fruits.length())
