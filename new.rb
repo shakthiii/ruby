c99ce7c6c0337878cc42148974e21b58070b8c0c
Sakthivel M <sakthivel@innoventes.co>
2022-12-16 10:54:03 +0530

    ==addition of two number==

--- new.rb
+++ new.rb
@@ -1,12 +1,37 @@
-name = "sakthi"
+# name = "sakthi"
 
-age =  "22"
+# age =  "22"
 
-puts ("My name is "+ name +" and my age is "+ age)
+# puts ("My name is "+ name +" and my age is "+ age)
 
-string = "My name is " + name + " and my age is "+age
+# string = "My name is " + name + " and my age is "+age
 
-puts "My name is #{name} and my age is #{age}"
+# puts "My name is #{name} and my age is #{age}"
 
 
-puts string
+# puts <<STRING
+
+# S
+# A
+# K
+# T
+# H
+# I
+# STRING
+
+#addition of two numbers
+
+puts ("Enter the number1")
+
+number1 = gets.chomp().to_i
+
+puts ("Enter the number2")
+
+
+number2 = gets.chomp().to_i
+
+# puts(number1 + number2)
+ 
+res = number1 + number2;
+
+puts ("The result is: " + res.to_s)
