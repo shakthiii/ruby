be00a2cbb115ea527ec2bab11f268e05b28f5b6d
Sakthivel M <sakthivel@innoventes.co>
2022-12-19 12:28:27 +0530

    ==create a simple calculator using if elsif else and relational operators ===

--- new.rb
+++ new.rb
@@ -131,3 +131,100 @@ user= true
 end
 
 conditonLoop()
+
+
+# using if statements with operators
+
+isMale = false
+isHieght = false
+
+if isMale and isHieght 
+    puts("You are a tall male")
+elsif isMale or isHieght
+    puts("you are a tall or male")
+else
+    puts("you are not a male and not so tall")
+end
+
+
+# Using if statements with relational operators!
+
+
+def maxOfNumbers(a, b, c)
+
+    if a>b and b>c 
+        return puts("#{a} is greater thean other numbers")
+    elsif b>c
+        return puts("#{b} is greater thean other numbers")
+    else
+        return puts("#{c} is greater thean other numbers")
+    end
+end
+
+
+maxOfNumbers(4, 8, 2)
+
+
+# Building a simple calculator in Ruby!
+
+puts("SIMPLE CALCULATOR")
+puts ("================================")
+puts ("1: ADD")
+puts ("2: SUBTRACT")
+puts ("3: MULTIPLY")
+puts ("4: DIVIDE")
+puts ("5: POWER")
+puts ("6: MODULO")
+
+puts ("Enter the first number")
+a = gets.chomp().to_i;
+puts ("Enter the second number")
+
+b = gets.chomp().to_i;
+
+puts ("Enter the Choice")
+
+choice = gets.chomp().to_i;
+
+
+
+
+def calculator(a,b, choice)
+
+                
+    if choice == 1
+puts ("================================")
+
+        return puts("Addition of #{a} and #{b} is #{a+b}")
+    elsif choice == 2
+puts ("================================")
+
+            return puts("Subraction of #{a} and #{b} is #{a-b}")
+    elsif choice==3
+puts ("================================")
+        
+        return puts("Multiplicaiton of #{a} and #{b} is #{a*b}")
+
+    elsif choice == 4
+puts ("================================")
+        
+        return puts("Division of #{a} and #{b} is #{a/b}")
+
+    elsif choice == 5
+        puts ("================================")
+        return puts ("Power of #{a} and #{b} is #{a**b}")
+
+    elsif choice == 6
+            puts ("================================")
+            return puts("Modulo of #{a} and #{b} is #{a%b}")
+
+    else
+puts ("================================")
+
+        puts ("Please choose a valid number")
+    end
+end
+
+
+
+calculator(a,b,choice)
