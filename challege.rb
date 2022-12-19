08c737fd58be57fec0fa61bb86c96535ad90b2f9
Sakthivel M <sakthivel@innoventes.co>
2022-12-19 11:41:34 +0530

    ==arrays, hashes, methods and conditional loops== and added a challenge file ===

---
+++ challege.rb
@@ -0,0 +1,100 @@
+=begin
+    1) Return the sum of two numbers
+This challenge calls on you to write 
+a function that adds together 
+two numbers within an argument. 
+For instance, if you have an argument 
+like this: (5, 4), you want your function 
+to add them to get their sum.
+=end
+
+def sumOfTwoNumbers(a,b) 
+
+    return a+b
+
+end
+
+
+puts (sumOfTwoNumbers(5,9))
+
+
+=begin
+
+2) Return the following number from 
+an integer passed
+In this problem, your goal is to create a 
+function that uses a number as an argument, 
+then increases it by a value of one. 
+For example, if the number in your argument 
+is 3, you want 4 to be returned, or if your 
+argument number is -8, you want the number 
+returned to be -7.
+
+=end
+
+
+def integerOfNumber(number)
+
+    return number + 1
+end
+
+
+puts (integerOfNumber(-8))
+
+
+=begin
+
+3) Reversing arrays
+your objective is to reverse the order of 
+the numbers in an array. For example, if 
+your array was [8, 2, 9, 4], you would want 
+your code to return [4, 9, 2, 8].
+
+=end
+
+def rev(array)
+
+    return array.reverse()
+end
+
+
+print (rev([8, 2, 9, 4]))
+
+
+
+=begin 
+4) Missing number game
+For this problem, you make a function that examines an array of 
+numbers ranging between 1 and 10 that’s missing one number and 
+have the function find the number that’s missing.
+For example, the series of numbers could be [2, 1, 5, 4, 6, 9, 7, 8, 10]. 
+This array is missing the number 3. The goal of this problem is to write code that 
+figures out that the number 3 is missing.
+=end
+
+def findEle(arr, ele)
+
+    puts (arr.include?(ele))
+    print (arr.sort())
+
+
+
+
+end
+
+number =  [2, 1, 5, 4, 6, 9, 7, 8, 10]
+
+number.map {|ele| ele*2}
+
+
+print(number)
+
+
+# findEle(number, 3)
+
+
+array = [1,2,3]
+array.each { |n| n * 2 }
+
+
+print(array)
--- new.rb
+++ new.rb
@@ -14,6 +14,9 @@
 # S
 # A
 # K
+
+
+
 # T
 # H
 # I
@@ -38,35 +41,93 @@
 
 #arrays in Ruby
 
-fruits = ['apple', 'orange', 'pineapple', 'mango']
+# fruits = ['apple', 'orange', 'pineapple', 'mango']
 
-fruits.pop
+# fruits.pop
 
-puts (fruits)
+# puts (fruits)
 
-fruits.shift
+# fruits.shift
 
-puts ('--------------------------------------------------------')
+# puts ('--------------------------------------------------------')
 
-puts (fruits)
+# puts (fruits)
 
-fruits.push('kiwi');
-puts ('--------------------------------------------------------')
+# fruits.push('kiwi');
+# puts ('--------------------------------------------------------')
 
-puts (fruits)
+# puts (fruits)
 
-fruits.unshift('strawberry');
-puts ('--------------------------------------------------------')
+# fruits.unshift('strawberry');
+# puts ('--------------------------------------------------------')
 
-puts (fruits)
-
-
-fruits.delete('strawberry');
-puts ('--------------------------------------------------------')
-
-puts (fruits)
+# puts (fruits)
 
 
-puts ('--------------------------------------------------------')
+# fruits.delete('strawberry');
+# puts ('--------------------------------------------------------')
 
-puts (fruits.length())
+# puts (fruits)
+
+
+# puts ('--------------------------------------------------------')
+
+# puts (fruits.length())
+
+
+# Hashes in Ruby ................
+
+states = {
+    :India => 'IN',
+    :America => 'USA',
+    :UnintedKingdom => 'UK',
+}
+
+puts (states[:America])
+
+
+# methods ................................
+
+def sayHello
+
+    puts ("Hello world User!")
+
+end
+
+sayHello
+
+
+def newMethod(name)
+    # puts("ENter your name here...")
+    # name = gets.chomp()
+    puts ("Hello " + name)
+
+end
+
+newMethod('sakthi')
+
+# Using comment
+
+=begin
+
+This is a multi-line comment!
+
+=end
+
+# Using methods and loops 
+
+def conditonLoop
+boolean = true
+user= true
+
+    if boolean and user
+        puts("User and boolean is true so Printed the result...")
+
+    else 
+        puts("User and boolean is false so Printed the result...")
+    end
+
+
+end
+
+conditonLoop()
